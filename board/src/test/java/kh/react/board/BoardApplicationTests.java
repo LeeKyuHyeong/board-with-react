b0cd648ccc24b9945ee1cbe67e33c3693f06bc13
leekh5 <rbgud2380@naver.com>
2024-12-10 09:36:29 +0900

    class setting

---
+++ board/src/test/java/kh/react/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.react.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
