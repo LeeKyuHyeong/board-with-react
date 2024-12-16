fc76d6d30fba6f1d86de82d5fe3ccca3028ccd34
rbgud <rbgud2380@naver.com>
2024-12-16 20:43:44 +0900

    정상화..

---
+++ board/frontend/src/component/common/Navbar.js
@@ -0,0 +1,87 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// 네비게이션 바 스타일 정의
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #333;
+  padding: 1rem 2rem;
+`;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+  color: white;
+  font-weight: bold;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 1.5rem;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  font-size: 1rem;
+
+  &:hover {
+    color: #ddd;
+  }
+`;
+
+const MobileMenuIcon = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    font-size: 1.5rem;
+    color: white;
+    cursor: pointer;
+  }
+`;
+
+const Navbar = ( { userData, sessionTime } ) => {
+
+  return (
+    <NavbarContainer>
+      <Logo><NavLink to="/" style={{"fontSize":"2rem"}}>MyApp</NavLink></Logo>
+      <NavLinks>
+				{userData ? (
+					<>
+          <span>Logged in as: {userData.name}, sessionTime Left: {sessionTime}s</span>
+					<NavLink to="/mypage">MyPage</NavLink>
+					<NavLink to="/logout">Logout</NavLink>
+					{userData.role === "superAdmin" ? (
+						<>
+						<NavLink to="/admin" userData={userData}>Admin</NavLink>
+						<NavLink to="/members" userData={userData}>List</NavLink>
+						</>
+					) : (
+								<>
+								{userData.role === "admin" ? (<><NavLink to="/members" userData={userData}>List</NavLink></>) : (<></>)}
+								</>
+							)}
+					</>
+        ) : (
+					<>
+					<NavLink to="/signup">SignUp</NavLink>
+					<NavLink to="/login">Login</NavLink>
+					</>
+        )}
+      </NavLinks>
+      <MobileMenuIcon>
+        <span>&#9776;</span>
+      </MobileMenuIcon>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
---
+++ board/frontend/src/component/member/SignUp.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const SignUpContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f4f4f4;
+`;
+
+const FormContainer = styled.div`
+  background-color: white;
+  padding: 2rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  width: 100%;
+  max-width: 400px;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 1.5rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.8rem;
+  margin: 0.8rem 0;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 0.8rem;
+  background-color: #333;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+const Select = styled.select`
+  width: 100%;
+  padding: 0.8rem;
+  margin: 0.8rem 0;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 1rem;
+`;
+
+const SignUp = () => {
+	const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+		role: ''
+  });
+
+	const [message, setMessage] = useState('');
+
+	const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+	const navi = useNavigate();
+
+	const handleSubmit = async (e) => {
+    e.preventDefault();
+    // 회원가입 처리 로직을 추가하면 됩니다.
+
+		try {
+      // Spring Boot 서버로 POST 요청
+			console.log('회원가입 데이터:', formData);
+
+      const response = await axios.post('http://localhost:8011/api/members/', formData);
+
+      setMessage(response.data); // 성공 메시지
+			navi('/login');
+    } catch (error) {
+      setMessage(error.response.data); // 에러 메시지
+    }
+  };
+
+
+	return (
+		<SignUpContainer>
+      <FormContainer>
+        <Title>Sign Up</Title>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            value={formData.name}
+            onChange={handleChange}
+						required
+          />
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+						required
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+						required
+          />
+					<Select name="role" required onChange={handleChange}>
+						<option value="">===select role===</option>
+						<option value="user">user</option>
+						<option value="admin">admin</option>
+					</Select>
+          <Button type="submit">Sign Up</Button>
+        </form>
+				{message && <p>{message}</p>}
+      </FormContainer>
+    </SignUpContainer>
+	);
+};
+
+export default SignUp;
