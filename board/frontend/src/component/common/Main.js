fbf67e3e5db1cde1d9b65717bf961d347b1af333
inBOB <rbgud2380@naver.com>
2024-12-27 15:14:38 +0900

    241227 last day in Hayang

--- board/frontend/src/component/common/Main.js
+++ board/frontend/src/component/common/Main.js
@@ -16,6 +16,9 @@ const Main = ( {userdata} ) => {
       <p>Nothing impossible</p>
       <p>It's new start fighting!!
       </p>
+      <p>It's nice to see you! see you later~
+      </p>
+
 			{errorMessage && <p style={styles.error}>{errorMessage}</p>}
 		</div>
 	);
